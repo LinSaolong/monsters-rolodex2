e36a29012751b41bf546229de2e2ae338f8fe937
LinSaolong <alongbcy@hotmail.com>
2020-02-18 14:30:59 +0700

    Updates

---
+++ precache-manifest.63e35ab5e5c52d9a07991185f9691bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da71855879130af7f1cf4c93266d6c6b",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "c4ceb1abce6a09187134",
+    "url": "/monsters-rolodex2/static/css/main.56e91ce7.chunk.css"
+  },
+  {
+    "revision": "a7b61fb692df94341236",
+    "url": "/monsters-rolodex2/static/js/2.5d0974b9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex2/static/js/2.5d0974b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ceb1abce6a09187134",
+    "url": "/monsters-rolodex2/static/js/main.233fc513.chunk.js"
+  },
+  {
+    "revision": "40da2808068ae7b3a450",
+    "url": "/monsters-rolodex2/static/js/runtime-main.b73997ea.js"
+  }
+]);
